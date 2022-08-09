20bee41f7ffb2bd26abe330f24b8fb17a04e8818
rLukoyanov <rulukoyanov@gmail.com>
2022-08-09 12:55:04 +0300

    feat(recipe): Adding Content to the Recipes Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  resipes = [];
+  resipes: Recipe[] = [
+    new Recipe(
+      'This is a Test Recipe',
+      'simple test text',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F31%2F16354-easy-meatloaf-mfs-74-1x1-1.jpg'
+    ),
+  ];
 
   constructor() {}
 
