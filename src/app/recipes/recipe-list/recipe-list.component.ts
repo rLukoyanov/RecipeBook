cbcaa382231e76668140ee7da6b65cd4d4108d9f
rLukoyanov <rulukoyanov@gmail.com>
2022-08-09 13:16:32 +0300

    feat(recipe): Displaying Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  resipes: Recipe[] = [
+  recipes: Recipe[] = [
+    new Recipe(
+      'This is a Test Recipe',
+      'simple test text',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F43%2F2022%2F03%2F31%2F16354-easy-meatloaf-mfs-74-1x1-1.jpg'
+    ),
     new Recipe(
       'This is a Test Recipe',
       'simple test text',
